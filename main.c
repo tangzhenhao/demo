1c9f44c6d8a4eda59a06555cf7389af7866cc22f
tangzhenhao <58258966+tangzhenhao@users.noreply.github.com>
2021-01-16 17:31:21 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 void npd1();
 void npd2();
-void npd3();
 void double_free();
 void div_by_zero();
 
 int main(int argc, char **args) {
   npd1();
   npd2();
-  npd3();
   double_free();
   div_by_zero();
 }
